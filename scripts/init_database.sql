c9f74a44457b6faa8fc47c796d055f2245e14c51
2nwarayman <157072310+2nwarayman@users.noreply.github.com>
2026-09-09 07:15:13 +0300

    Initialize DataWarehouse database and schemas
    This script initializes the DataWarehouse database by checking for its existence, dropping it if it exists, and creating a new database along with its schema layers.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,40 @@
+-- Use the master database
+USE master;
+GO
 
+-- Check if DataWarehouse already exists
+IF EXISTS (
+    SELECT 1
+    FROM sys.databases
+    WHERE name = 'DataWarehouse'
+)
+BEGIN
+    -- Allow only one connection and disconnect existing users
+    ALTER DATABASE DataWarehouse
+    SET SINGLE_USER
+    WITH ROLLBACK IMMEDIATE;
+
+    -- Delete the existing database
+    DROP DATABASE DataWarehouse;
+END;
+GO
+
+-- Create a new DataWarehouse database
+CREATE DATABASE DataWarehouse;
+GO
+
+-- Use the newly created database
+USE DataWarehouse;
+GO
+
+-- Create the Bronze layer for raw data
+CREATE SCHEMA bronze;
+GO
+
+-- Create the Silver layer for cleaned/transformed data
+CREATE SCHEMA silver;
+GO
+
+-- Create the Gold layer for business-ready data
+CREATE SCHEMA gold;
+GO
